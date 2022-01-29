b46750065460543cdcd5cd03c6d29a33c91fbb9a
Nicolas Dimitri <nicolasdodimitri@gmail.com>
2022-01-29 19:28:30 -0300

    funcionalidade do search implementada

--- src/API/index.js
+++ src/API/index.js
@@ -2,6 +2,6 @@ export const KEY = "058958e15fc14b248a2202419222901";
 
 export function searchAPI(key, city) {
   return fetch(
-    `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`
+    `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=1&aqi=no&alerts=no`
   ).then((r) => r.json());
 }
--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import { searchAPI, KEY } from "./API";
 import "./App.css";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import AppContext from "./context/AppContext";
 
 function App() {
   const [input, setInput] = useState("");
-  const [search, setSearch] = useState('')
-  console.log(searchAPI(KEY, search));
+  const [search, setSearch] = useState("");
+  const { weatherData, setWeatherData } = useContext(AppContext);
+  console.log(weatherData);
   return (
     <div>
       <h1>Chill Weather</h1>
+      <h2>{}</h2>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          setSearch(input);
+          searchAPI(KEY, input).then((r) => setWeatherData(r));
         }}
       >
-        <input 
-        value={input}
-        onChange={({target:{value}}) => setInput(value)}
-        type="text" />
+        <input
+          value={input}
+          onChange={({ target: { value } }) => setInput(value)}
+          type="text"
+        />
         <button type="submit">enter</button>
       </form>
     </div>
