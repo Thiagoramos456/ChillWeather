1e94ae8baf41368c80fde9d83ee82c46dd58e817
Nicolas Dimitri <nicolasdodimitri@gmail.com>
2022-02-10 19:07:04 -0300

    features

--- src/App.js
+++ src/App.js
@@ -7,21 +7,18 @@ import LanguageSelect from './components/LanguageSelect';
 import LocationInput from './components/LocationInput';
 
 function App() {
-<<<<<<< HEAD
   const [input, setInput] = useState("");
   const { weatherData, setWeatherData, language, setCity, city } = useContext(AppContext);
   // useEffect(() => {
   //   searchAPI(KEY, city, language).then(r => setWeatherData(r))
   // }, []);
-=======
-  const [input, setInput] = useState('');
-  const [search, setSearch] = useState('');
-  const { weatherData, setWeatherData } = useContext(AppContext);
+  // const [input, setInput] = useState('');
+  // const [search, setSearch] = useState('');
+  // const { weatherData, setWeatherData } = useContext(AppContext);
 
-  useEffect(() => {
-    console.log(weatherData);
-  }, [weatherData]);
->>>>>>> 3fd24b5a650ca318414047da82b3e02681954661
+  // useEffect(() => {
+  //   console.log(weatherData);
+  // }, [weatherData]);
   return (
     <div>
       <LocationInput />
